c01f96ba8c07aff03e7568039cde3297c86e38a5
kawehner <kwehner@umich.edu>
2023-12-08 18:41:02 -0500

    edit

--- billboard_.py
+++ billboard_.py
@@ -3,27 +3,60 @@ import sqlite3
 import json
 import os
 import billboard
+import re
+import matplotlib.pyplot as plt
+import csv
+
+from bs4 import BeautifulSoup
+import requests
 
 
-# def billboard_hot_100():
-#     # hot 100 songs --> song name, song ranking, artist, (song id??)
-#     chart_data = billboard.ChartData('hot-100')
-#     hot_100_data = []
-#     for i in range(0,len(chart_data)):
-#         song_name = chart_data[1].title
-#         #song_id = chart_data[]
-#         song_ranking = chart_data[1].rank
-#         song_artist = chart_data[1].artist
+def create_data_base(database_name):
+    db_name = f'{database_name}.db'
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+    return cur, conn
 
 
-# info needed: song name, ranking, artist, (song id??)
+def billboard_hot_100(date, cur, conn):
+    song_list = {}
+    
+    chart = billboard.ChartData('hot-100', date)
 
-# top 100 songs during Dec 2019 (e.g. Dec 1, 2019) & Dec 2020 (e.g. Dec 1, 2020)
+        # rank, song title, artist
+        # cur --> where you specify what you want it to do
+    year = date[:3]
+    table_name = f"Billboard_Hot_100_{year}"  # concatenate the table name w/ the date variable
+    create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} (Rank NUMBER PRIMARY KEY, Title TEXT, Artist TEXT)"
+    cur.execute(create_table_query)
+    # conn.commit()
 
-# sort the data so that it's the top 25 songs (then 50, 75, 100) --> store 25 items in the database for each run
+    cur.execute(f"SELECT MAX(Rank) FROM {table_name}")
+    temp = cur.fetchone()[0]
+    print(temp)
+    if not temp:
+        index = 0
+    else:
+        index = int(temp)
+        #   print(index)
+        # info about top 100 songs
+    for i in range(index,index+25):
+        song_per_date = []
+
+        song_title = chart[i].title
+        song_artist = chart[i].artist
+        song_rank = chart[i].rank 
+            # tuple
+            #song_per_date.append((song_title, song_artist, song_rank))
+            #song_list[date] = song_per_date
+        cur.execute(f"INSERT OR IGNORE INTO {table_name} (Rank, Title, Artist) VALUES (?,?,?)",
+            (song_rank, song_title, song_artist)) 
+
+        conn.commit()
+       
 
 
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## downloading a chart ##
 # ChartData --> constructor to download chart
 # e.g. ChartData(name, date=None, year=None, fetch=True, timeout=25)
@@ -55,3 +88,21 @@ import billboard
 # weeks – The number of weeks the track has been or was on the chart, including future dates (up until the present time).
 # !!! rank – The track's current position on the chart.
 # isNew – Whether the track is new to the chart.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def main():
+    # create_data_base --> create database for billboard data
+    database_name = 'Billboard_Hot_100_Database'
+    cur, conn = create_data_base(database_name)
+    
+
+    # billboard_hot_100()
+    date_list = ['2019-12-01', '2020-12-01']
+    for date in date_list:
+        hot_100_songs = billboard_hot_100(date, cur, conn)
+    
+
+main()
+
+
