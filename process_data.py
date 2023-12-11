5894f9f886db330231ed1880bd0d41d353216efe
kawehner <kwehner@umich.edu>
2023-12-11 16:40:26 -0500

    edit

--- process_data.py
+++ process_data.py
@@ -1,5 +1,9 @@
 
 
 # calculate average song valence level 
+# def average_valence():
+    # parameter --> pass in valence output of spotipy function 
 
 # calculate average song danceability level 
+# def average_danceability():
+    # parameter --> pass in danceability output of spotipy function
